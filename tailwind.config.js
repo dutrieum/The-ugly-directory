2da97df2029c6e0e3e6016e7bd3fdce72101ac62
dutrieum <dutrieuxmathilde@gmail.com>
2022-11-14 21:13:49 +0100

    Feat: add typos, colors, logo and header template

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,27 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
+    fontFamily: {
+      'serif': ['Arvo', 'serif'],
+      'sans': ['Nunito', 'sans-serif'],
+    },
+    fontSize: {
+      '22': '22px',
+      '30': '30px',
+      '32': '32px',
+      '36': '36px',
+      '60': '60px',
+      '80': '80px',
+      '130': '130px'
+    },
+    colors: {
+      'green': 'var(--color-green)',
+      'light-green': 'var(--color-light-green)',
+      'dark-green': 'var(--color-dark-green)',
+      'black': 'var(--color-black)',
+      'gray': 'var(--color-gray)',
+      'white': 'var(--color-white)',
+    },
     extend: {},
   },
   plugins: [],
