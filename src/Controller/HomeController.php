e84abbd47952232645a1da9e3d13a76a0a31132b
dutrieum <dutrieuxmathilde@gmail.com>
2022-11-15 19:47:49 +0100

    Feat: end of home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VillagerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(VillagerRepository $villagerRepository): Response
     {
         return $this->render('home/index.html.twig', [
+            'villagers' => $villagerRepository->findAll(),
             'controller_name' => 'HomeController',
         ]);
     }
