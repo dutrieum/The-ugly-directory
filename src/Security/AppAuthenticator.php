cb6aadad781abba895a12f3f16c78a2898d8d78c
dutrieum <dutrieuxmathilde@gmail.com>
2022-11-17 19:34:28 +0100

    Feat: add edit and delete button on villager page if permission

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -46,7 +46,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
     protected function getLoginUrl(Request $request): string
